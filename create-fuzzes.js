6f590d2a710c599a37f0d4a7ed88a693ca5eaa0f
Mojtaba Eshghie <mojtaba.eshghi@ut.ac.ir>
2020-12-27 08:15:55 -0800

    all good, tx's are generated!

--- create-fuzzes.js
+++ create-fuzzes.js
@@ -3,13 +3,17 @@ const Web3 = require('web3')
 const glob = require("glob")
 const fs = require('fs')
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const get_abi = require('./get-abi');
 
 let web3 = new Web3(Web3.givenProvider || "ws://localhost:33333");
 
-let numberOfTXsForEachContractSet = 1 // put 10 here
+let numberOfTXsForEachContractSet = 10 // put 10 here
 let safety = null;
 let file_name = null;
 let source_code = null;
+let tx_array = []
+let current_generated_txs = 25;
+let lock = false;
 
 
 let files_to_use = [
@@ -25,32 +29,105 @@ let files_to_use = [
     }
 ]
 
+
+vulnerable_set_attacker_abi = get_abi('vulnerables.sol');
+safe_set_attacker_abi = get_abi('safes.sol');
+
+
 let makeCreateTXFuzz = () => {
     files_to_use.forEach((file_object, index) => {
         safety = file_object['mode'];
         file_name = file_object['file_name']
-    
-        //source_code = fs.readFileSync('contracts/'+file_name);
-    
+        
+        
+
         if (safety == "vul") {
-    
-            deploy(file_name, 'Vulnerable', file_object['value_for_sending_to_victim']).then(addr => {
-                console.log(addr);
-            })
-            deploy(file_name, 'GiveMeEverything').then(addr => {
-                console.log(addr);
-            })
+            
+            deploy(file_name, 'Vulnerable', file_object['value_for_sending_to_victim'])
+                .then(async (victim_addr) => {
+                    return {
+                        victim_addr: victim_addr,
+                        attacker_addr: await deploy(file_name, 'GiveMeEverything')
+                    };
+                })
+                .then(addr_object => {
+                    let tx_object = {
+                        "files": 'vulnerables.sol',
+                        "attacker": {
+                            "address": addr_object["attacker_addr"],
+                            "abi": vulnerable_set_attacker_abi
+                        },
+                        "victim": {
+                            "contract_name": "Vulnerable",
+                            "address": addr_object["victim_addr"],
+                        },
+                        "label": "vul"
+                    }
+                    
+                    let thisInterval = setInterval(() => {
+                        if (lock == false) {
+                            lock = true;
+                            console.log("Locked the critical section.")
+                            clearInterval(thisInterval);
+                            current_generated_txs++;
+                            the_json_txs_object = JSON.parse(fs.readFileSync('params/tx_fuzz.json'))
+                            the_json_txs_object[current_generated_txs.toString()] = tx_object;
+                            fs.writeFileSync('params/tx_fuzz.json', JSON.stringify(the_json_txs_object, null, 4))
+                            console.log("Releasing the lock......")
+                            lock = false;
+                        } else {
+                            console.log("critical section was locked, waiting...")
+                            return;
+                        }
+                    }, 1000)
+                    
+
+                });
+                    
+
+            
+                    
     
         } else if (safety == "safe") {
-    
-            deploy(file_name, 'Safe', file_object['value_for_sending_to_victim']).then(addr => {
-                console.log(addr);
-            })
-    
-            deploy(file_name, 'GiveMeEverything').then(addr => {
-                console.log(addr);
-            })
-    
+
+            deploy(file_name, 'Safe', file_object['value_for_sending_to_victim'])
+                .then(async (victim_addr) => {
+                    return {
+                        victim_addr: victim_addr,
+                        attacker_addr: await deploy(file_name, 'GiveMeEverything')
+                    };
+                })
+                .then(addr_object => {
+                    let tx_object = {
+                        "files": 'safes.sol',
+                        "attacker": {
+                            "address": addr_object["attacker_addr"],
+                            "abi": safe_set_attacker_abi
+                        },
+                        "victim": {
+                            "contract_name": "Safe",
+                            "address": addr_object["victim_addr"],
+                        },
+                        "label": "safe"
+                    }
+
+                    let thisInterval = setInterval(() => {
+                        if (lock == false) {
+                            lock = true;
+                            console.log("Locked the critical section.")
+                            clearInterval(thisInterval);
+                            current_generated_txs++;
+                            the_json_txs_object = JSON.parse(fs.readFileSync('params/tx_fuzz.json'))
+                            the_json_txs_object[current_generated_txs.toString()] = tx_object;
+                            fs.writeFileSync('params/tx_fuzz.json', JSON.stringify(the_json_txs_object, null, 4))
+                            console.log("Releasing the lock......")
+                            lock = false;
+                        } else {
+                            console.log("critical section was locked, waiting...")
+                            return;
+                        }
+                    }, 1000)
+                });
         }
     })
 }
@@ -60,3 +137,5 @@ let makeCreateTXFuzz = () => {
 for (var i = 1; i <= numberOfTXsForEachContractSet; i++) {
     makeCreateTXFuzz()
 }
+
+
